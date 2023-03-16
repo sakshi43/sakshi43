973f41401b9f581776bf4d89193176eedab015bb
sakshi43 <109756349+sakshi43@users.noreply.github.com>
2023-03-16 12:00:55 +0530

    Add files via upload

---
+++ Userstate.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Userstate() {
+
+    return ( 
+        <div >
+        <h1 > hello </h1> 
+        <input type = "text" value = "name" > </input> </div>
+    )
+}
+export default  Userstate;
